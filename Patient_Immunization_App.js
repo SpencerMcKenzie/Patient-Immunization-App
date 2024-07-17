9739420be19d6d5f16dc41d2a508de2ab26f99d7
SpencerMcKenzie <spencermckenzie@me.com>
2024-07-17 16:58:54 -0500

    Create Patient_Immunization_App.js

---
+++ Patient_Immunization_App.js
@@ -0,0 +1,53 @@
+
+
+// DDX Bricks Wiki - See https://developer.domo.com/docs/ddx-bricks/getting-started-using-ddx-bricks
+// for tips on getting started, linking to Domo data and debugging your app
+ 
+//Available globals
+var domo = window.domo; // For more on domo.js: https://developer.domo.com/docs/dev-studio-guides/domo-js#domo.get
+var datasets = window.datasets;
+
+//Step 1. Select your dataset(s) from the button in the bottom left corner
+
+
+
+//Step 2. Query your dataset(s): https://developer.domo.com/docs/dev-studio-references/data-api
+var fields = ['PharmacyLocation','FacName','PharmID','PatientName','PatBirthDate'];
+var groupby = ['PharmacyLocation','FacName','PharmID','PatientName','PatBirthDate'];
+var query = `/data/v1/${datasets[0]}?fields=${fields.join()}&groupby=${groupby.join()}`;
+domo.get(query).then(handleResult);
+
+
+
+//Step 3. Do something with the data from the query result
+function handleResult(data){
+  console && console.log(data);
+}
+
+/*ORIGINAL DOMO CODE */
+
+/*
+function handleResult(data){
+
+//console.log("handleResult data: ",data);
+
+// let result = data.map(a => a.PatientName);
+  let result = data.map(o => { return {PharmacyLocation: o.Column1, FacName: o.FacName ,PatientName: o.PatientName,PatBirthDate: o.PatBirthDate} })
+  console.log("result: ",result);
+
+  var table = new Tabulator("#example-table", {
+   data:result,
+   //renderHorizontal:"virtual",
+   autoColumns:true,
+   height:"311px",
+   layout:"fitColumns",
+   //layout:"fitDataTable",
+   //layout:"fitDataStretch",
+   columns:[
+       {title:"Default Pharmacy Location", field:0}, //column has a fixed width of 100px;
+       {title:"Patient Name", field:"1"},//column will be allocated 1/5 of the remaining space
+       {title:"BirthDate", field:"2"}, //column will be allocated 3/5 of the remaining space
+       {title:"FacName", field:"3"} // column has a default widthGrow of 1 and will be allocated 1/5 of the remaining space
+    ],
+    });
+    */ 
